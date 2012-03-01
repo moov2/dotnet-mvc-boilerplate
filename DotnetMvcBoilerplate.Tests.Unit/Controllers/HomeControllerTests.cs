7680a953e3d2c2e70a8398bac601f3171c2257e4
Peter Keating <peter@moov2.com>
2012-03-01 12:43:34 +0000

    added NUnit and a sample test that is not automated via the build script.

---
+++ DotnetMvcBoilerplate.Tests.Unit/Controllers/HomeControllerTests.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using NUnit.Framework;
+using DotnetMvcBoilerplate.Controllers;
+using System.Web.Mvc;
+
+namespace DotnetMvcBoilerplate.Tests.Unit.Controllers
+{
+    public class HomeControllerTests
+    {
+        /// <summary>
+        /// Tests that Index returns the view.
+        /// </summary>
+        [Test]
+        public void Index_ReturnsViewResult()
+        {
+            Assert.That(new HomeController().Index(), Is.InstanceOf<ViewResult>());
+        }
+    }
+}
--- README.md
+++ README.md
@@ -6,8 +6,10 @@ Project that offers a starting point for ASP.NET MVC projects.
 
 Below are a list of all the third party frameworks & tools used in the boilerplate.
 
+* ASP.NET MVC 3
 * jQuery - 1.7.1
 * Modernizr - 2.5.3
 * NAntContrib - 0.91 RC 1
 * lesscss - 1.2.0
 * lessc - Feb 15th
+* NUnit - 2.6.0.12054
