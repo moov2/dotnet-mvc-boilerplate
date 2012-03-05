4f98a6bba8d0ff129a9833352f00f732d02f6ac1
Peter Keating <peter@moov2.com>
2012-03-05 17:34:56 +0000

    added display & css for installation form that will enable the user to create the primary administration account.

---
+++ DotnetMvcBoilerplate/Controllers/InstallController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Web.Mvc;
+
+namespace DotnetMvcBoilerplate.Controllers
+{
+    public class InstallController : Controller
+    {
+        /// <summary>
+        /// Displays the Install form that allows the
+        /// user to create the initial admin account.
+        /// </summary>
+        /// <returns>Form for creating initial admin.</returns>
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
