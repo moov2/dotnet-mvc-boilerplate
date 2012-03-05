e05c37f94eb837876469cdfe7f41459b29ead726
Peter Keating <peter@moov2.com>
2012-03-05 18:15:20 +0000

    added validation to the view model to ensure that the ConfirmPassword is equal to the Password value.

---
+++ DotnetMvcBoilerplate.Tests.Unit/ViewModels/Install/InstallViewModelTests.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Linq;
+using NUnit.Framework;
+using DotnetMvcBoilerplate.ViewModels.Install;
+using DataAnnotationsExtensions;
+
+namespace DotnetMvcBoilerplate.Tests.Unit.ViewModels.Install
+{
+    public class InstallViewModelTests
+    {
+        /// <summary>
+        /// Tests that validation is done to ensure that the ConfirmPassword
+        /// is equal to Password.
+        /// </summary>
+        [Test]
+        public void Validation_ConfirmPasswordIsEqualToPassword()
+        {
+            var propertyInfo = typeof(InstallViewModel).GetProperty("ConfirmPassword");
+
+            var attribute = propertyInfo.GetCustomAttributes(typeof(EqualToAttribute), false)
+                                          .Cast<EqualToAttribute>()
+                                          .FirstOrDefault();
+
+            Assert.That(attribute.OtherProperty, Is.EqualTo("Password"));
+        }
+    }
+}
--- DotnetMvcBoilerplate/ViewModels/Install/InstallViewModel.cs
+++ DotnetMvcBoilerplate/ViewModels/Install/InstallViewModel.cs
@@ -1,9 +1,11 @@
 ﻿using System;
+using DataAnnotationsExtensions;
 
 namespace DotnetMvcBoilerplate.ViewModels.Install
 {
     public class InstallViewModel
     {
+        [EqualTo("Password", ErrorMessage = "Password confirmation doesn't match Password.")]
         public string ConfirmPassword { get; set; }
         public string Username { get; set; }
         public string Password { get; set; }
