b1700f5b683b594d5c1aa95dedd0ad9badd72088
Peter Keating <peter@moov2.com>
2012-03-12 16:58:41 +0000

    session is re-populated with user data when the users session starts again because they selected remember me when logging in.

--- DotnetMvcBoilerplate/Global.asax.cs
+++ DotnetMvcBoilerplate/Global.asax.cs
@@ -4,6 +4,9 @@ using MvcTurbine.ComponentModel;
 using MvcTurbine.Unity;
 using DotnetMvcBoilerplate.Core.IO;
 using DotnetMvcBoilerplate.Core.Security;
+using DotnetMvcBoilerplate.Models;
+using DotnetMvcBoilerplate.Core.Service;
+using System.Web;
 
 namespace DotnetMvcBoilerplate
 {
@@ -27,6 +30,22 @@ namespace DotnetMvcBoilerplate
         {
             if (IsAuthenticated)
                 ServiceLocator.Resolve<ISessionAuthentication>().SetRoles();
+        }
+
+        /// <summary>
+        /// Gets the User object from the database.
+        /// </summary>
+        /// <returns>User that is logged in.</returns>
+        private User GetLoggedInUser()
+        {
+            var userService = ServiceLocator.Resolve<IUserService>();
+            return userService.ById(HttpContext.Current.User.Identity.Name);
+        }
+
+        protected void Session_Start(Object sender, EventArgs e)
+        {
+            if (IsAuthenticated)
+                ServiceLocator.Resolve<SessionAuthentication>().SetSessionData(GetLoggedInUser()); 
         }
 
         public override void Startup()
