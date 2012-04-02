0d4acd79b2d27e26f3dc2b37b3903775ad58e189
Peter Keating <peter@moov2.com>
2012-04-02 16:24:00 +0100

    now when Mongo isn't installed on the running machine, the user is redirected to a page explaining why they are viewing the page and how to correct the situation.

---
+++ DotnetMvcBoilerplate/Controllers/MongoNotInstalledController.cs
@@ -0,0 +1,12 @@
+﻿using System.Web.Mvc;
+
+namespace DotnetMvcBoilerplate.Controllers
+{
+    public class MongoNotInstalledController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
--- DotnetMvcBoilerplate/Global.asax.cs
+++ DotnetMvcBoilerplate/Global.asax.cs
@@ -7,17 +7,32 @@ using DotnetMvcBoilerplate.Core.Security;
 using DotnetMvcBoilerplate.Models;
 using DotnetMvcBoilerplate.Core.Service;
 using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+using DotnetMvcBoilerplate.Controllers;
 
 namespace DotnetMvcBoilerplate
 {
     public class MvcApplication : TurbineApplication
     {
+        private const string MongoNotInstalledUrl = "/MongoNotInstalled";
+
         /// <summary>
         /// Flag indicating if the user is logged in.
         /// </summary>
         public bool IsAuthenticated
         {
             get { return Context.User != null && Context.User.Identity.IsAuthenticated; }
+        }
+
+        /// <summary>
+        /// Flag indicating whether MongoDB is installed
+        /// as a Windows Service on the server machine.
+        /// </summary>
+        public bool MongoInstalled
+        {
+            get { return (bool)Application["MongoInstalled"]; }
+            set { Application["MongoInstalled"] = value; }
         }
 
         public MvcApplication()
@@ -32,6 +47,12 @@ namespace DotnetMvcBoilerplate
                 ServiceLocator.Resolve<ISessionAuthentication>().SetRoles();
         }
 
+        protected void Application_BeginRequest(Object sender, EventArgs e)
+        {
+            if (!MongoInstalled && !NavigatingToMongoNotInstalled() && IsPageRequest())
+                Response.Redirect(MongoNotInstalledUrl);
+        }
+
         /// <summary>
         /// Gets the User object from the database.
         /// </summary>
@@ -42,6 +63,27 @@ namespace DotnetMvcBoilerplate
             return userService.ById(HttpContext.Current.User.Identity.Name);
         }
 
+        /// <summary>
+        /// Returns flag indicating whether the request is for
+        /// a web page.
+        /// </summary>
+        /// <returns>True if Request is for a page, otherwise false.</returns>
+        private bool IsPageRequest()
+        {
+            return Request.CurrentExecutionFilePathExtension == "";
+        }
+
+        /// <summary>
+        /// Returns true if the Request is to the MongoNotInstalled
+        /// page, otherwise false will be returned.
+        /// </summary>
+        /// <returns>True if navigation to mongo not install
+        /// page, otherwise false.</returns>
+        private bool NavigatingToMongoNotInstalled()
+        {
+            return Request.Url.LocalPath == MongoNotInstalledUrl;
+        }
+
         protected void Session_Start(Object sender, EventArgs e)
         {
             if (IsAuthenticated)
@@ -50,7 +92,7 @@ namespace DotnetMvcBoilerplate
 
         public override void Startup()
         {
-            WindowsService.StartIfNotRunning(WindowsService.MongoDB);
+            MongoInstalled = WindowsService.StartIfNotRunning(WindowsService.MongoDB);
         }
     }
 }
